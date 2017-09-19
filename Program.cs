b55c62c07e9ec7c773ba5fdace2b5947ee727a5f
Morten Primdahl <morten@primdahl.dk>
2017-09-19 22:32:02 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TodoApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
+            BuildWebHost(args,url).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args, string url) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls(url)
+                .Build();
+    }
+}
